b240198521b1a516446fe2ddcdf4b10002c0cfa9
Yng314 <chowyoung314@gmail.com>
2026-02-19 16:40:08 +0000

    Initial Commit

---
+++ readytoorder/readytoorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  readytoorderApp.swift
+//  readytoorder
+//
+//  Created by Young on 2026/2/19.
+//
+
+import SwiftUI
+
+@main
+struct readytoorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
